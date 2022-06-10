2e005aa7c87efbecd6c7786b5c1a1ccc5aadd1a9
William Robert-Coyle <willrobertcoyle22@gmail.com>
2022-06-10 17:37:28 -0500

    Modification section post, alignement centre

--- front-page.php
+++ front-page.php
@@ -1,20 +1,23 @@
-<main>
     <div class="hero">
         <?php get_header(); ?> 
         <div class="hero__titre">
             <h1>Modèle Wordpress</h1>
         </div>
     </div>
-        <?php
-        if (have_posts()):
-            while(have_posts()) : the_post(); ?>
-                <h2><?php the_title(); ?></h2>
-
-                <?php $lien = " <a class='bouton' href='" . get_permalink() . "'>". substr(get_the_title(), 0, 8) ." </a><hr>"; ?>
-                <p><?= wp_trim_words(get_the_content(), 20, $lien); ?></p>
-
-            <?php endwhile; ?>
-        <?php endif; ?>    
+    <div class="container">
+            <?php
+            if (have_posts()):
+                
+                while(have_posts()) : the_post(); ?>
+               
+                    <h2><?php the_title(); ?></h2>
+                    
+                    <?php $lien = " <br><br><a class='bouton' href='" . get_permalink() . "'>". substr(get_the_title(), 0, 8) ." </a><hr>"; ?>
+                    <p><?= wp_trim_words(get_the_content(), 20, $lien); ?></p>
+                 
+                <?php endwhile; ?>
+                
+            <?php endif; ?>    
+            </div>
 
         <?php get_footer(); ?>
-</main>
