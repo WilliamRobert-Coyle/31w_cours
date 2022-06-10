60264ae331739db1ed40380aaa58ad5c6511d7e2
William Robert-Coyle <willrobertcoyle22@gmail.com>
2022-06-10 17:45:58 -0500

    Ajout hr pour divisier les sections

--- single.php
+++ single.php
@@ -7,7 +7,7 @@
 if (have_posts()):
     while(have_posts()) : the_post(); ?>
         <h2><?php the_title(); ?></h2>
-        <p><?php the_content();?></p>
+        <p><hr><?php the_content();?><hr></p>
         <?php
             $titre = get_the_title();
             $sigle = substr($titre, 0, 8);
